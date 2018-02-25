2db14da6794ecdb745f93a897c2c06aed7f634fd
unknown <syyadkhan@yahoo.com>
2018-02-25 17:29:05 -0500

    feb251728

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -6,14 +6,17 @@ function resetGame () {
     hangmanState = 0;
     drawWord(gameShownAnswer);    
 }
-$(document).ready(resetGame);
+function newFunction() {
+    $(document).ready(resetGame);
+}
+
 function win () { alert('You win!');  resetGame() ;}
 function lose () { alert('Oh no, you lose!'); resetGame(); }
 function doKeypress () {
     var tempChar = $('#letter-input').val().toLowerCase();
     var tempString = "";
     $('#letter-input').val("");
-
+    
     // Write here!
     tempString = guessLetter( tempChar, gameShownAnswer, gameAnswer );
     if ( tempString != gameShownAnswer ) {
@@ -21,13 +24,13 @@ function doKeypress () {
         gameShownAnswer = tempString;
         if ( gameShownAnswer === gameAnswer ) {
             win();
-}
+        }
     } else {
         wrongLetter(tempChar);
         drawSequence[ hangmanState++ ]();
         if ( hangmanState === drawSequence.length ) {
             lose();
+        }
+    }  
 }
-    }
-}
-$('#letter-input').keyup( doKeypress );
+
