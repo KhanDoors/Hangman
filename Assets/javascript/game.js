319bb931cb1bc9cf70dd761346d7dd655d74f47e
unknown <syyadkhan@yahoo.com>
2018-03-02 09:27:08 -0500

    mar20927

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -4,6 +4,22 @@ window.onload = function () {
   'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
   't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+  // create alphabet ul
+  var buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
+
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
+
 //define words
 var teamList = ["cowboys", "giants", "eagles", "redskins"];
 
