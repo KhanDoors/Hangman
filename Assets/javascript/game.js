7dca686874ffd4e9f0dd0194ee2b3ded5ddd4993
unknown <syyadkhan@yahoo.com>
2018-03-02 12:08:18 -0500

    mar21208

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,27 +1,12 @@
 window.onload = function () {
 
- var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-  't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-  // create alphabet ul
-  var buttons = function () {
-    myButtons = document.getElementById('buttons');
-    letters = document.createElement('ul');
-
-    for (var i = 0; i < alphabet.length; i++) {
-      letters.id = 'alphabet';
-      list = document.createElement('li');
-      list.id = 'letter';
-      list.innerHTML = alphabet[i];
-      check();
-      myButtons.appendChild(letters);
-      letters.appendChild(list);
-    }
-  }
-
+   
 //define words
-var teamList = ["cowboys", "giants", "eagles", "redskins"];
+var words = ["cowboys", "giants", "eagles", "redskins"];
+
+var word=words[Math.floor(Math.random()* words.length)];
+
+var answer=[];
 
 var guessLeft = 9;
 
@@ -44,20 +29,20 @@ var rightGuessDashes = [];
 
 //randomly select the word
 function getWord() { 
-    chosenWord = teamList[Math.floor(Math.random() * teamList.length)];
+    chosenWord = words[Math.floor(Math.random() * words.length)];
     document.getElementById("Words").innerHTML = chosenWord;
 };
 //execute above function
 getWord();
 //create dashes for amount of letters
-var answerArray = [];
+var answer=[];
 
-for (var i = 0; i <teamList.length; i++) {
-
-    answerArray[i] = "_";
+for (var i=0; i<word.length;i++) 
+{
+    answer[i]="_";
       }
 
-  document.getElementById("spaces").innerHTML = answerArray;
+  document.getElementById("spaces").innerHTML = answer;
 //get user input
 var guess = prompt("Guess a letter, or click Cancel to stop playing.");
 
@@ -82,8 +67,30 @@ for (var j = 0; j < word.length; j++) {
    }
 
  }
-
   
+ // remaining guess
+ var remainingGuess=word.length;
+while(remainingGuess>0){
+  alert(answer.join(" "));
+  var guess=prompt("Guess a letter or cancel to quit game");
+  if (guess==null){
+    break;
+  }
+  else if (guess.length!=1){
+    alert("please enter a single letter");
+  }
+  else {
+    for (var j=0;j<word.length;j++){
+      if(word[j]==guess){
+        answer[j]=guess;
+        remainingGuess--;
+      }
+    }
+  }
+}
+alert(answer.join(" "));
+alert("You Won!"+ teamlist);
+
 //compare user input to the word
 function Search() {
     if (guess == answerArray) {
@@ -92,6 +99,8 @@ function Search() {
         alert("Uh-Oh");
     }
 }
+
+
 //win or lose
 
 //
